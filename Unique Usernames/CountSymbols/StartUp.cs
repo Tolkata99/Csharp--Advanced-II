50affe47809d0ba627e869989c68073adbc2fa23
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-06-06 11:58:30 +0300

    I solved CountSymbols from Sets and Dictionaries Advanced - Exercise!

---
+++ Unique Usernames/CountSymbols/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> ch = new Dictionary<char, int>();
+
+            char[] tokens = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if(!ch.ContainsKey(tokens[i]))
+                {
+                    ch.Add(tokens[i], 0);
+                }
+
+                ch[tokens[i]]++;
+            }
+
+            foreach (var (chKey,timesValue) in ch.OrderBy(c => c.Key))
+            {
+                Console.WriteLine($"{chKey}: {timesValue} time/s");
+            }
+        }
+    }
+}
