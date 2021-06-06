4d90d4141a5ea8204f095bb3a504a4789d11b1d8
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-06-06 11:59:09 +0300

    .

---
+++ Unique Usernames/Sets and Dictionaries Advanced - Exercise/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets_and_Dictionaries_Advanced___Exercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> names = new SortedSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
