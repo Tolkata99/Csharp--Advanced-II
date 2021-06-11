b4f6ba0226418acd342f1dadabb7d011e1bdc26a
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-06-11 14:34:27 +0300

    .

---
+++ Streams, Files and Directories/ForParts/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ForParts
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using FileStream stream = new FileStream(@"..\..\..\TextForParts.txt",FileMode.Open);
+            var parts = 4;
+
+           
+
+            var leghtbyte = (int)Math.Ceiling(stream.Length / (decimal)parts);
+
+            var buffer = new byte[leghtbyte];
+
+            for (int i = 0; i < parts; i++)
+            {
+                var byteread = stream.Read(buffer, 0, buffer.Length);
+
+                using var currentPart = new FileStream($"Part{i + 1}.txt",FileMode.OpenOrCreate);
+
+                currentPart.Write(buffer, 0, buffer.Length);
+
+
+            }
+
+        }
+    }
+}
---
+++ Streams, Files and Directories/Streams, Files and Directories/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Streams__Files_and_Directories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader reader = new StreamReader(@"..\..\..\text1.txt");
+            int counter = 0;
+
+
+            while (true)
+            {
+                var lline = reader.ReadLine();
+
+                if(lline == null)
+                {
+                    break;
+                }
+                
+
+
+                if(counter % 2 == 1)
+                {
+                    Console.WriteLine(lline);
+                }
+
+
+                counter++;
+            }
+        }
+    }
+}
